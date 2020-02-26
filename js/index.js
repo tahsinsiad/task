a118b9e1b65c1788db25ae9ceaf25089237ed022
tahsinsiad <tak.siad16@gmail.com>
2020-02-26 13:47:45 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
-var movingObj = document.getElementById("moving");
-document.getElementById("moving").style.left='0px';
-document.getElementById("moving").style.top='0px';
-setInterval(function() {	
-	document.getElementById("moving").style.left = parseInt(document.getElementById("moving").style.left)+50 +'px';
-	document.getElementById("moving").style.top = parseInt(document.getElementById("moving").style.top)+50 +'px';
-}, 1000)
+function movingBox() {
+	var elem = document.getElementById("moving-box");
+	var targetPx = 50;
+	var pos = 0;
+	var reversePos = window.innerHeight - 100 ;
+	setInterval(
+		function(){
+			if (pos >= window.innerHeight - 100) {
+				reversePos = reversePos - targetPx;
+				elem.style.top = reversePos + 'px'; 
+				elem.style.left = reversePos + 'px';
+				if(reversePos <= 0) {
+					pos = 0;
+					reversePos = window.innerHeight - 100;
+				}
+			} else {
+				pos = pos + targetPx;
+				elem.style.top = pos + 'px'; 
+				elem.style.left = pos + 'px'; 
+			}
+		}, 200)
+  }
+
+  function changeBgColor() {
+	  var elem = document.getElementById("moving-box");
+	  var colors = ["#43a7e0", "#de1818", "#25de18"];
+	  var index = 0;
+	  setInterval(function(){
+		  elem.style.background = colors[index++];
+		  if(index > colors.length){
+			  index=0;
+		  }
+	  }, 3000)
+  }
